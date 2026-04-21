a81fe3347d29509d01f3d97e41448a9e50de242e
Chyenne8 <chyenner444@gmail.com>
2026-04-20 21:09:54 -0400

    Updated EmotionMatch.tsx

--- src/components/challenges/EmotionMatch.tsx
+++ src/components/challenges/EmotionMatch.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { motion } from 'motion/react';
+import { motion } from "framer-motion";
 import { Smile, Frown, Sparkles } from 'lucide-react';
 import { GameData } from '../../types/game';
 
