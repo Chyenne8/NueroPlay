29584d0629df10812cc24d668ec08abb46f8132c
Chyenne8 <chyenner444@gmail.com>
2026-04-20 21:07:04 -0400

    Updated KidDashboard.tsx

--- src/pages/KidDashboard.tsx
+++ src/pages/KidDashboard.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
-import { motion } from 'motion/react';
+import { motion } from "framer-motion";
 // Lucide React Icons
 import {
   Star,
