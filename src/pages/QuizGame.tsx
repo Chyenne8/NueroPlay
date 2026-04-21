d9b93ae6022da459b50cfb1ad81e7e0c9afe357f
Chyenne8 <chyenner444@gmail.com>
2026-04-20 21:50:56 -0400

    Updated QuizGame.tsx

--- src/pages/QuizGame.tsx
+++ src/pages/QuizGame.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { challenges } from '../components/challenges';
 import { ProgressBar } from '../components/ProgressBar';
