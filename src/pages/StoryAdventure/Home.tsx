ee24fa475f49e1fd314de6e0f82c2041f37d59a1
Chyenne8 <chyenner444@gmail.com>
2026-04-20 21:33:08 -0400

    Updated Home.tsx

--- src/pages/StoryAdventure/Home.tsx
+++ src/pages/StoryAdventure/Home.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import { Palette, Heart, Users, Repeat, BookOpen, ArrowLeft } from "lucide-react";
 
 export default function StoryAdventureHome() {
