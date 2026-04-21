3b1923502571cbb2025a39f5910cd05ff5324557
Chyenne8 <chyenner444@gmail.com>
2026-04-20 21:18:29 -0400

    Updated MultiStepInstruction.tsx

--- src/components/challenges/MultiStepInstruction.tsx
+++ src/components/challenges/MultiStepInstruction.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { motion, AnimatePresence } from 'motion/react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Star, Circle } from 'lucide-react';
 import { GameData } from '../../types/game';
 
