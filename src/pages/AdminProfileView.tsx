dc23f4c89ada01a8ded473a1092c4792c602e4fe
Chyenne8 <chyenner444@gmail.com>
2026-04-20 21:49:21 -0400

    Updated AdminProfileView.tsx

--- src/pages/AdminProfileView.tsx
+++ src/pages/AdminProfileView.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router-dom';
 import { 
   Brain, 
   Palette, 
