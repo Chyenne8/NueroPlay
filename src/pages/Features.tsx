0c339e5421bfc6f21a3e09bba72ced6acedb2a5b
Chyenne8 <chyenner444@gmail.com>
2026-04-20 21:50:34 -0400

    Updated Features.tsx

--- src/pages/Features.tsx
+++ src/pages/Features.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import { Card } from '../components/ui/card';
 import { Button } from '../components/ui/button';
 import { 
